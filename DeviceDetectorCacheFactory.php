41a191779386c060872ccc79a14edaa1eabd4c7a
Thomas Steur <tsteur@users.noreply.github.com>
2019-08-15 08:33:09 +1200

    revert last change

--- DeviceDetectorCacheFactory.php
+++ DeviceDetectorCacheFactory.php
@@ -12,18 +12,12 @@ use Piwik\DeviceDetector\DeviceDetectorFactory;
 
 class DeviceDetectorCacheFactory extends DeviceDetectorFactory
 {
-    private $cache = array();
-
-    public function makeInstance($userAgent)
+    protected function getDeviceDetectionInfo($userAgent)
     {
-        if (isset($this->cache[$userAgent])) {
-            return $this->cache[$userAgent];
-        }
         if (DeviceDetectorCacheEntry::isCached($userAgent)) {
-            $this->cache[$userAgent] = new DeviceDetectorCacheEntry($userAgent);
-            return $this->cache[$userAgent];
+            return new DeviceDetectorCacheEntry($userAgent);
         }
 
-        return parent::makeInstance($userAgent);
+        return parent::getDeviceDetectionInfo($userAgent);
     }
 }
