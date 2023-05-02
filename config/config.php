57cd574e03d16b6fc577788a29b3d581bfd6102b
Stefan Giehl <stefan@matomo.org>
2023-05-02 11:25:34 +0200

    Apply required changes for vendor proxies (#58)
    * use new vendor proxies
    
    * adjust console commands

--- config/config.php
+++ config/config.php
@@ -4,5 +4,5 @@ return [
     'DeviceDetectorCacheIgnoreUserAgentsWithLessThanXRequests' => 9,
     'DeviceDetectorCacheNumLinesToScan' => 5000000,
     \Piwik\DeviceDetector\DeviceDetectorFactory::class
-        => DI\autowire(\Piwik\Plugins\DeviceDetectorCache\Factory::class),
+        => Piwik\DI::autowire(\Piwik\Plugins\DeviceDetectorCache\Factory::class),
 ];
--- config/test.php
+++ config/test.php
@@ -4,7 +4,7 @@ use Piwik\Plugins\DeviceDetectorCache\CachedEntry;
 
 return [
     'DeviceDetectorCacheIgnoreUserAgentsWithLessThanXRequests' => 0,
-    \Piwik\DeviceDetector\DeviceDetectorFactory::class => DI\decorate(function ($previous) {
+    \Piwik\DeviceDetector\DeviceDetectorFactory::class => Piwik\DI::decorate(function ($previous) {
         CachedEntry::setCacheDir(PIWIK_DOCUMENT_ROOT . '/tmp/devicecachetests/');
 
         return $previous;
