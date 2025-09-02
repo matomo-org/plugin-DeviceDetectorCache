897a1a7802e3710bbce11729f2f189b93a8276f4
Stefan Giehl <stefan@matomo.org>
2025-09-02 16:54:46 +0200

    Updates expected test files after core change (#81)
    * update expected test files
    
    * fix tests for older releases
    
    * adjust version

--- tests/System/DeviceDetectorCacheTest.php
+++ tests/System/DeviceDetectorCacheTest.php
@@ -11,6 +11,7 @@ namespace Piwik\Plugins\DeviceDetectorCache\tests\System;
 
 use Piwik\Plugins\DeviceDetectorCache\tests\Fixtures\SimpleFixtureTrackFewVisits;
 use Piwik\Tests\Framework\TestCase\SystemTestCase;
+use Piwik\Version;
 
 /**
  * @group DeviceDetectorCache
@@ -38,6 +39,18 @@ class DeviceDetectorCacheTest extends SystemTestCase
             'Live.getLastVisitsDetails',
         ];
 
+        $columnsToHide = [];
+
+        if (version_compare(Version::VERSION, '5.2.0-alpha', '<')) {
+            // In Matomo 5.2 referrer columns had been added to ecommerce actions. For tests with older Matomo releases we therefor ignore those columns
+            $columnsToHide = array_merge($columnsToHide, ['referrerType', 'referrerName', 'referrerKeyword']);
+        }
+
+        if (version_compare(Version::VERSION, '5.5.0-b1', '<')) {
+            // In Matomo 5.5 ai referrer had been added
+            $columnsToHide = array_merge($columnsToHide, ['referrerAIAssistantUrl', 'referrerAIAssistantIcon']);
+        }
+
         $apiToTest   = [];
         $apiToTest[] = [
             $api,
@@ -46,8 +59,7 @@ class DeviceDetectorCacheTest extends SystemTestCase
                 'date'       => self::$fixture->dateTime,
                 'periods'    => ['day'],
                 'testSuffix' => '',
-                // fields were added in Matomo 5.2-alpha, so ignore them as they are not relevant for this plugin anyways
-                'xmlFieldsToRemove' => ['referrerType', 'referrerName', 'referrerKeyword']
+                'xmlFieldsToRemove' => $columnsToHide,
             ],
         ];
 
